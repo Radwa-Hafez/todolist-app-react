a526cedec726bf5471ef1f293c2833b2d6d267a3
Radwa-Hafez <139390139+Radwa-Hafez@users.noreply.github.com>
2023-10-12 22:53:21 +0200

    Added the form of the Todolist app

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const TodoForm = () => {
-  return <div></div>;
+  return (
+    <form className="TodoForm">
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="What is the task today?"
+      />
+      <button type="submit" className="todo-btn">
+        Add Task
+      </button>
+    </form>
+  );
 };
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { TodoForm } from "./TodoForm";
 
 export const TodoWrapper = () => {
-  return <div></div>;
+  return (
+    <div className="TodoWrapper">
+      <TodoForm />
+    </div>
+  );
 };
