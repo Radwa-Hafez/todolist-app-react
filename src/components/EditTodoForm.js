3d561a6ba62a08be321df0a0f19f548c8b545e59
Radwa-Hafez <139390139+Radwa-Hafez@users.noreply.github.com>
2023-10-13 17:33:28 +0200

    Made the app functional

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const EditTodoForm = () => {
-  return <div></div>;
+export const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    editTodo(value, task.id);
+    setValue("");
+  }
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="Update Task"
+        onChange={handleChange}
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
 };
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    addTodo(value);
+    setValue("");
+  }
   return (
-    <form className="TodoForm">
+    <form className="TodoForm" onSubmit={handleSubmit}>
       <input
         type="text"
         className="todo-input"
+        value={value}
         placeholder="What is the task today?"
+        onChange={handleChange}
       />
       <button type="submit" className="todo-btn">
         Add Task
